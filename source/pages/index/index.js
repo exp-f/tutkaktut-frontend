a7e6d66f5c387a01cdf951c03f0c279570f462ff
artem <rus.exp.field@yandex.ru>
2018-10-12 10:25:55 +0400

    added domcontentloaded rm urls

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -4,8 +4,7 @@ import { Russian } from 'flatpickr/dist/l10n/ru';
 import 'flatpickr/dist/flatpickr.css';
 import './index.scss';
 
-const url = 'http://127.0.0.1:8000/ru/';
-const closeDropdown = (e, a) => {
+const closeDropdown = (e) => {
   if (!e.target.matches('.block_1_select_dropdown > *')) {
     document.querySelector('.open').classList.remove('open');
     document.querySelector('.block_1_select_dropdown').style.maxHeight = '';
@@ -23,111 +22,114 @@ const buildDropdown = (list, container) => {
   });
 };
 
-document.querySelector('.block_1_selected').addEventListener('click', (e) => {
-  e.stopPropagation();
-  const container = e.target.parentElement;
-  const dropdown = container.querySelector('.block_1_select_dropdown');
-  let newHeight = 20;
-  [...dropdown.children].forEach((i) => {
-    newHeight += i.offsetHeight;
-  });
-  dropdown.style.maxHeight = `${newHeight}px`;
-  container.classList.add('open');
-  document.body.addEventListener('click', closeDropdown);
-});
 
-document.querySelectorAll('.block_1_select_dropdown_item').forEach((item) => {
-  item.addEventListener('click', (e) => {
-    const container = e.target.parentElement.parentElement;
-    const valueHandler = container.querySelector('.block_1_selected');
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.block_1_selected').addEventListener('click', (e) => {
+    e.stopPropagation();
+    const container = e.target.parentElement;
     const dropdown = container.querySelector('.block_1_select_dropdown');
-    const input = container.querySelector('input');
-    const newText = e.target.innerText;
-    const newValue = e.target.getAttribute('data-value');
-    valueHandler.innerText = newText;
-    input.value = newValue;
-    dropdown.style.maxHeight = '';
-    container.classList.remove('open');
-    document.body.removeEventListener('click', closeDropdown);
+    let newHeight = 20;
+    [...dropdown.children].forEach((i) => {
+      newHeight += i.offsetHeight;
+    });
+    dropdown.style.maxHeight = `${newHeight}px`;
+    container.classList.add('open');
+    document.body.addEventListener('click', closeDropdown);
   });
-});
 
-flatpickr(document.getElementById('date'), {
-  locale: Russian,
-  enableTime: true,
-  time_24hr: true,
-  dateFormat: 'd.m.Y H:i',
-  defaultDate: new Date(),
-});
+  document.querySelectorAll('.block_1_select_dropdown_item').forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const container = e.target.parentElement.parentElement;
+      const valueHandler = container.querySelector('.block_1_selected');
+      const dropdown = container.querySelector('.block_1_select_dropdown');
+      const input = container.querySelector('input');
+      const newText = e.target.innerText;
+      const newValue = e.target.getAttribute('data-value');
+      valueHandler.innerText = newText;
+      input.value = newValue;
+      dropdown.style.maxHeight = '';
+      container.classList.remove('open');
+      document.body.removeEventListener('click', closeDropdown);
+    });
+  });
 
-let inputTimeout = null;
+  flatpickr(document.getElementById('date'), {
+    locale: Russian,
+    enableTime: true,
+    time_24hr: true,
+    dateFormat: 'd.m.Y H:i',
+    defaultDate: new Date(),
+  });
 
-const searchAddress = (e) => {
-  if (inputTimeout) clearTimeout(inputTimeout);
-  const query = e.target.value;
-  if (!query) return false;
-  inputTimeout = setTimeout(() => {
-    fetch(`${url}/api/streets?city_id=14&term=${query}`, {
+  let inputTimeout = null;
+
+  const searchAddress = (e) => {
+    if (inputTimeout) clearTimeout(inputTimeout);
+    const query = e.target.value;
+    if (!query) return false;
+    inputTimeout = setTimeout(() => {
+      fetch(`api/streets?city_id=14&term=${query}`, {
+        credentials: 'include',
+      })
+        .then(r => r.json())
+        .then((r) => {
+          e.target.style.zIndex = '11';
+          buildDropdown(r, e.target.parentElement.querySelector('.js-address'));
+        });
+    }, 200);
+    return true;
+  };
+
+  document.querySelector('[name="street_from"]').addEventListener('input', searchAddress);
+  document.querySelector('[name="street_to"]').addEventListener('input', searchAddress);
+
+  document.body.addEventListener('click', (e) => {
+    for (let element = e.target; element !== null; element = element.parentElement) {
+      if (element.matches('.js-custom-dropdown')) {
+        const input = element.parentElement.parentElement.querySelector('input[type="text"]');
+        input.value = element.innerText;
+        input.style.zIndex = '';
+        element.parentElement.innerHTML = '';
+        break;
+      }
+    }
+  });
+
+  document.getElementById('delivery_form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const orderStr = `${document.querySelector('.block_1_selected').innerText} / ${formData.get('datetime')} /` +
+      ` С ${formData.get('street_from')}, ${formData.get('house_from')}, кв ${formData.get('appartment_from')}` +
+      ` / До ${formData.get('street_to')}, ${formData.get('house_to')}, кв ${formData.get('appartment_to')}`;
+    console.log(orderStr);
+    const popup = document.querySelector('.popup');
+    popup.querySelector('.popup_text').innerText = orderStr;
+    popup.parentElement.classList.add('open');
+  });
+
+  document.querySelector('.popup_close').addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.classList.remove('open');
+  });
+
+  document.getElementById('finalPopup').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const finalFormData = new FormData(e.target);
+    const formData = new FormData(document.getElementById('delivery_form'));
+    if (!finalFormData.has('agree')) return console.log('Вы должны согласиться на обработку персональных данных');
+    const datetime = formData.get('datetime').split(' ');
+    formData.set('phone', finalFormData.get('phone'));
+    // formData.set('name', finalFormData.get('name'));
+    formData.set('comment', finalFormData.get('comment'));
+    formData.set('date', datetime[0]);
+    formData.set('time', datetime[1]);
+    return fetch(`${document.getElementById('delivery_form').getAttribute('action')}`, {
+      method: 'post',
+      body: formData,
       credentials: 'include',
     })
       .then(r => r.json())
       .then((r) => {
-        e.target.style.zIndex = '11';
-        buildDropdown(r, e.target.parentElement.querySelector('.js-address'));
+        console.log(r);
       });
-  }, 200);
-  return true;
-};
-
-document.querySelector('[name="street_from"]').addEventListener('input', searchAddress);
-document.querySelector('[name="street_to"]').addEventListener('input', searchAddress);
-
-document.body.addEventListener('click', (e) => {
-  for (let element = e.target; element !== null; element = element.parentElement) {
-    if (element.matches('.js-custom-dropdown')) {
-      const input = element.parentElement.parentElement.querySelector('input[type="text"]');
-      input.value = element.innerText;
-      input.style.zIndex = '';
-      element.parentElement.innerHTML = '';
-      break;
-    }
-  }
-});
-
-document.getElementById('delivery_form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const orderStr = `${document.querySelector('.block_1_selected').innerText} / ${formData.get('datetime')} /` +
-    ` С ${formData.get('street_from')}, ${formData.get('house_from')}, кв ${formData.get('appartment_from')}` +
-    ` / До ${formData.get('street_to')}, ${formData.get('house_to')}, кв ${formData.get('appartment_to')}`;
-  console.log(orderStr);
-  const popup = document.querySelector('.popup');
-  popup.querySelector('.popup_text').innerText = orderStr;
-  popup.parentElement.classList.add('open');
-});
-
-document.querySelector('.popup_close').addEventListener('click', (e) => {
-  e.target.parentElement.parentElement.classList.remove('open');
-});
-
-document.getElementById('finalPopup').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const finalFormData = new FormData(e.target);
-  const formData = new FormData(document.getElementById('delivery_form'));
-  if (!finalFormData.has('agree')) return console.log('Вы должны согласиться на обработку персональных данных');
-  const datetime = formData.get('datetime').split(' ');
-  formData.set('phone', finalFormData.get('phone'));
-  // formData.set('name', finalFormData.get('name'));
-  formData.set('comment', finalFormData.get('comment'));
-  formData.set('date', datetime[0]);
-  formData.set('time', datetime[1]);
-  return fetch(`${url}${document.getElementById('delivery_form').getAttribute('action')}`, {
-    method: 'post',
-    body: formData,
-    credentials: 'include',
-  })
-    .then(r => r.json())
-    .then((r) => {
-      console.log(r);
-    });
+  });
 });
